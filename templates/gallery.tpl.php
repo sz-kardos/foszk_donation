9429117a7ea802a2be8d3b8ced49d4a70311cec1
taben88 <c0D1n91988@gmail.com>
2024-04-11 00:52:38 +0200

    Képfájlok megjelennek a galériában.

--- templates/gallery.tpl.php
+++ templates/gallery.tpl.php
@@ -1,5 +1,5 @@
 <?php
-$logged_in = !(empty($_SESSION["loggedInAs"]));
+$logged_in = !(empty($_SESSION["logged_in_as"]));
 $action = $LOGICAL_LINKS['upload_attempt'];
 if($logged_in){
     echo "<form action=${action} method='post' enctype='multipart/form-data'>
@@ -9,5 +9,14 @@ if($logged_in){
     </form>";
 }
 
-
+$paths = glob($IMAGES_PATH . "/*");
+echo "<div class='gallery'>";
+foreach ($paths as $path){
+    if(checkIfImage($path)){
+        $filename = basename($path);
+        $link = $IMAGES_ROOT_LINK."/".$filename;
+        echo sprintf("<img class='gallery-item' src='%s' height='256px'>", $link);
+    }
+}
+echo "</div>";
 ?>
